bbf9ad9b8376a7d1b7e2b0df64f21c2bfe2023ad
Vindhya R <vindhyarao2013@gmail.com>
2025-01-21 20:06:01 -0500

    Redeploy to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware - cors - allows to parse json
+//middleware - cors - allows to parse json.
 app.use(cors());
 app.use(express.json());
 
